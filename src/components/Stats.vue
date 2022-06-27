fa7eee0fc083e10a8e1f51a88855408bfdf8711f
carl louis manuel <carlxaeron@users.noreply.github.com>
2022-06-28 07:34:45 +0800

    moved to stats component

---
+++ src/components/Stats.vue
@@ -0,0 +1,24 @@
+<template>
+    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
+    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
+    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+    name: "Stats",
+    props: {
+        recaps: String,
+        playersByTeam: String,
+    },
+    setup(props) {
+
+    }
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/components/layouts/GameBody.vue
+++ src/components/layouts/GameBody.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="pt-6 gameBody">
-    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
-    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
-    <comparer title="Possession" leftText="1" rightText="2" :leftPercent="33" :rightPercent="55"/>
+    <stats :recaps="recaps" :playersByTeam="playersByTeam" />
   </div>
 </template>
 
 <script lang='ts'>
 import Comparer from '@/components/Comparer.vue';
 import { defineComponent, computed } from 'vue';
+import Stats from '../Stats.vue';
 
 export default defineComponent({
-  components: { Comparer },
+  components: { Comparer, Stats },
   name: 'GameBody',
   props: {
     recaps: String,
